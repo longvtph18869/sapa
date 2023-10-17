cc33b69f6077087266dac618643baba527a5f4b4
longvtph18869 <longvutien15092000@gmail.com>
2023-10-17 14:07:16 +0700

    commit

---
+++ assets/js/video.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".video_btn").magnificPopup({
+    disableOn: 50,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
+});
